0df79dbf3d6a83b60674af2537f97b9ab0563745
Antério Vieira da Silva <anteriovieira@gmail.com>
2017-06-29 18:40:16 -0300

    Template removido

--- portal/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import * as ng from 'angular';
-
-let mod = ng.module('app.components', []).name;
-
-// Registre seus componentes
-// module.component('nomeDoComponente', {} )
-
-export default mod;
--- portal/src/services/index.js
+++
@@ -1,5 +0,0 @@
-import * as ng from 'angular';
-
-export default ng.module('app.services', [
-    // Registre seus módulos
-]).name;
